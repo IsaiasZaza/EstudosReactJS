a933c477d2d45ee3055fe1b8012c9b082c876e6b
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-11-04 14:56:46 -0300

    essa e a mensagem

---
+++ src/app/components/Circle.tsx
@@ -0,0 +1,7 @@
+function Circle (){
+    return (
+        <h1 className="text-center">circulo</h1>
+    )
+}
+
+export default Circle;
---
+++ src/app/components/Square.tsx
@@ -0,0 +1,14 @@
+import Circle from "./Circle";
+
+function Quadrado() {
+    return (
+        <>
+            <div className="w-52 h-52 bg-orange-700 text-white">
+                texto qualquer
+            </div>
+            <Circle />
+        </>
+    );
+}
+
+export default Quadrado;
