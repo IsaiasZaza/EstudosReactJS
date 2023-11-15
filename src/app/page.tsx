cb8f9b898947329f76a27ca888ae71f8cde952af
IsaiasZaza <131933508+IsaiasZaza@users.noreply.github.com>
2023-11-15 00:58:17 -0300

    editando lista de tarefas, adicionando o css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,78 @@
 "use client"
 
 import { Person } from "@/types/Person";
+import { TodoItem } from "@/types/TodoItem";
 import { FormEvent, useState } from "react";
 
 function Page() {
-   
-  const [alterar, setAlterar] = useState({name: '', sobrenome: '', place: 'Digite'})
 
- 
-  const click = () => {
-    setAlterar({...alterar, name: '', sobrenome: '', place: 'Digite novamente'})
-  
+  const [itemInput, setItemInput] = useState('');
+
+  //adicionar
+  const [list, setList] = useState<TodoItem[]>([
+
+    { label: 'Fazer dever', checked: false },
+    { label: 'Fazer nada', checked: false }
+
+  ]);
+  const adicionarLi = () => {
+
+    //verificação de label vazia
+    if (itemInput.trim() === '') {
+      alert('Por favor preencha o campo');
+      return;
+    }
+    setList([
+      ...list, { label: itemInput, checked: false }]);
+    setItemInput('');
   }
 
+  //deletar
+  const deleteItem = (index: number) => {
+    setList(list.filter((item, key) => key !== index))
+  }
+
+  //ALTERAR
+  const alterarCheck = (index: number) => {
+    let newList = [...list];
+
+    newList[index].checked = !newList[index].checked;
+    setList(newList);
+  }
 
   return (
-    <div>
-      <input type="text" 
-      placeholder={alterar.place}
-      value={alterar.name}
-      onChange={e => setAlterar({...alterar, name: e.target.value})} 
-      />
+    <>
+      <div className="centro">
+          <div>
+            <h1>Lista de tarefas</h1>
+          </div>
+          <div>
+            <div className="beleza">
+              <input type="text"
+                placeholder="O que deseja fazer?"
+                value={itemInput}
+                onChange={e => setItemInput(e.target.value)}
+              />
+              <button onClick={adicionarLi}>Adicionar</button>
+            </div>
+          </div>
+        
 
-      <input type="text"
-      placeholder={alterar.place}
-      value={alterar.sobrenome}
-      onChange={e => setAlterar({...alterar, sobrenome: e.target.value })}  
-      />
-      <p>O nome todo do seu personagem é</p>
-      <p>{alterar.name} {alterar.sobrenome}</p>  
-      <button onClick={click}>eae</button>
-    </div> 
+        <p>{list.length} itens na lista</p>
+        <ul>
+          {list.map((item, index) =>
+          (<li key={index}>
+            <input onClick={() => alterarCheck(index)} type="checkbox" checked={item.checked} />
+            {item.label} - <button onClick={() => deleteItem(index)}>[ deletar ]</button></li>))}
+        </ul>
+      </div>
+     
+    </>
   )
-    
+
 }
 
 export default Page;
 
 
- 
+
